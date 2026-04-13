f59a72d24c57c3ce6c6030a1d4583dc08bf7efbf
courds <willcolleyfs@gmail.com>
2026-04-13 16:31:39 +0100

    Update strava-refresh.js

--- api/strava-refresh.js
+++ api/strava-refresh.js
@@ -1,14 +1,11 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
-
   const { refresh_token } = req.body;
-
   if (!refresh_token) {
     return res.status(400).json({ error: 'No refresh token' });
   }
-
   try {
     const response = await fetch('https://www.strava.com/oauth/token', {
       method: 'POST',
@@ -20,7 +17,6 @@ export default async function handler(req, res) {
         grant_type: 'refresh_token',
       }),
     });
-
     const data = await response.json();
     res.json({
       access_token: data.access_token,
